d53bd8f85f7ac2735cdd81ea12f3675b309f8cb9
gustonecrush <codeboxezz@gmail.com>
2023-02-10 22:30:31 +0700

    initialized project

---
+++ FetchAPI/FetchAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchAPIApp.swift
+//  FetchAPI
+//
+//  Created by A Farhan Agustiansyah on 10/02/23.
+//
+
+import SwiftUI
+
+@main
+struct FetchAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
