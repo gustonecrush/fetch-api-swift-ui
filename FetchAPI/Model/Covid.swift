1eee3ffb86e15aa69e9b15fd5acf18b7e412fc56
gustonecrush <codeboxezz@gmail.com>
2023-02-10 22:36:04 +0700

    created response body

---
+++ FetchAPI/Model/Covid.swift
@@ -0,0 +1,24 @@
+//
+//  Covid.swift
+//  FetchAPI
+//
+//  Created by A Farhan Agustiansyah on 10/02/23.
+//
+
+import Foundation
+import SwiftUI
+
+struct Total: Codable {
+    let jumlah_positif: Int
+    let jumlah_meninggal: Int
+    let jumlah_sembuh: Int
+    let jumlah_dirawat: Int
+}
+
+struct Update: Codable {
+    let total: Total
+}
+
+struct CovidResponse: Codable {
+    var update: Update
+}
