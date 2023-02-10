3d995bcff6d17b719fd5ebe543db67adaf99cd79
gustonecrush <codeboxezz@gmail.com>
2023-02-10 22:43:50 +0700

    create api service

---
+++ FetchAPI/API/APIService.swift
@@ -0,0 +1,32 @@
+//
+//  APIService.swift
+//  FetchAPI
+//
+//  Created by A Farhan Agustiansyah on 10/02/23.
+//
+
+import Foundation
+import SwiftUI
+
+class APIService: ObservableObject {
+    @Published var datatotal = [Total]()
+    
+    init() {
+        guard let url = URL(string: "https://data.covid19.go.id/public/api/update.json") else {
+            fatalError("Invalid URL")
+        }
+        
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard let data = data, error == nil else {
+                return
+            }
+            
+            let result = try? JSONDecoder().decode(CovidResponse.self, from: data)
+            if let result = result {
+                DispatchQueue.main.async {
+                    self.datatotal = [result.update.total]
+                }
+            }
+        }.resume()
+    }
+}
